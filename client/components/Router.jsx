560f30e0cab3a3f77c3cebc6cbdd007d18a77ce9
AlexanderGusevWeb <djuff89@gmail.com>
2019-09-05 12:10:20 +0300

    Updated files

--- client/components/Router.jsx
+++ client/components/Router.jsx
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 import App from "./App.jsx";
 import OtherLinks from "./OtherLinks.jsx";
 
-
 function AppRouter() {
     return (
         <Router>
