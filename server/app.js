5b3e528517f75bddef541d10318ff2c3d535f1f8
AlexanderGusevWeb <djuff89@gmail.com>
2019-09-05 19:50:32 +0300

    Updated files

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("bodyParser");
+const bodyParser = require("body-parser");
 
 const serverPort = require("../etc/config.json");
 
